2af2cc5bd266458fda4028790ad220fa8ad68d1f
tjdcjf1996 <r_louis@naver.com>
2024-12-03 12:54:16 +0900

    refactor: 체크 라우트 설정

--- src/routes/check.router.js
+++ src/routes/check.router.js
@@ -1,10 +1,12 @@
-import express from "express";
-import { CheckController } from "../controllers/check.controller.js";
-
+import express from 'express';
+import { CheckController } from '../controllers/check.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
 const checkController = new CheckController();
 
-// router.post("/이름뭘로짓지", checkController.이름뭘로짓지);
+router.get('/svrStatus', authMiddleware, checkController.getSvrStatus);
+router.get('/availableSvr', authMiddleware, checkController.getAvailableSvr);
+router.post('/svrStatus', authMiddleware, checkController.setSvrStatus);
 
 export default router;
