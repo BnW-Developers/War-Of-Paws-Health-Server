0fa1a8a76f6c2f8b83d8c33ce796725582b52519
tjdcjf1996 <r_louis@naver.com>
2024-12-03 15:26:19 +0900

    fix: bcrypt 비동기처리

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ export default async function (req, _, next) {
       // 토큰이 없을 시 에러
       throw new CustomErr('요청한 사용자의 API-KEY가 없습니다.', 401);
 
-    if (!checkHashed(authorization))
+    if (!(await checkHashed(authorization)))
       // 지정된 키와 다를 경우
       throw new CustomErr('일치하지 않은 API-KEY 정보입니다.', 401);
 
--- src/utils/auth/checkHashed.util.js
+++ src/utils/auth/checkHashed.util.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import { config } from '../../config/config.js';
 
-export const checkHashed = (hash) => {
-  return bcrypt.compare(config.auth.key, hash);
+export const checkHashed = async (hash) => {
+  return await bcrypt.compare(config.auth.key, hash);
 };
